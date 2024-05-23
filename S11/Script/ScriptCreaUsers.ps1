a8aebdc139c1495fd5cf1794c4a0446a027cdc8c
Nicolas Claverie <claverie.nicolas.lapo@gmail.com>
2024-05-23 22:18:29 +0200

    finalisation du script de creation d'utilisateur

--- S11/Script/ScriptCreaUsers.ps1
+++ S11/Script/ScriptCreaUsers.ps1
@@ -2,10 +2,10 @@
 #Import-Module ActiveDirectory
 
 # Chemin vers le fichier CSV
-$csvPath = "C:\Users\Administrator\ScriptAD\s09_Ekoloclast.csv"
+$csvPath = "C:\Users\Administrator\ScriptAD\s09_Ekoloclast.csv" # A modifier si besoin
 
 # Lire le fichier CSV
-$users = Import-Csv -Path $csvPath -Delimiter ";" -Header "Prenom", "Nom", "Societe", "Site", "Departement", "Service", "fonction", "Managerprenom", "Managernom", "PC", "Datedenaissance", "Telephonefixe", "Telephoneportable", "NomadismeTeletravail", "OUPrincipale", "FonctionModif"
+$users = Import-Csv -Path $csvPath -Delimiter ";" -Header "Prenom", "Nom", "Societe", "Site", "Departement", "Service", "fonction", "Managerprenom", "Managernom", "PC", "Datedenaissance", "Telephonefixe", "Telephoneportable", "NomadismeTeletravail", "OUPrincipale", "FonctionModif" | Select-Object -Skip 1
 
 # mise en variable du domaine
 $DomainDN = (Get-ADDomain).DistinguishedName
@@ -36,60 +36,95 @@ foreach ($user in $users) {
 
     
     # Vérifier l'existence de l'utilisateur et ajuster si nécessaire
-    #while (Get-ADUser -Filter { SamAccountName -eq $samAccountName }) {
-    #   
-    #  $samAccountName = "$baseSamAccountName$i"
-    # $i++
-    #}
+    while (Get-ADUser -Filter { SamAccountName -eq $samAccountName }) {
+        $samAccountName = "$baseSamAccountName$i"
+        $i++
+    }
 
     $displayName = "$prenom $Nom"
-    $userPrincipalName = "$samAccountName@$($user.Societe).fr"
+    $userPrincipalName = "$samAccountName@Ekoloclast.fr" # A modifier si besoin
     $password = "Azerty1*"  # Remplacer par une méthode plus sécurisée de gestion des mots de passe
 
     # Construire le chemin de l'OU
-    $ouPath = "OU=$($user.Service),OU=$($user.Departement),$DomainDN"
+    $ouPath = "OU=$Service,OU=$Departement,OU=LabUtilisateurs,$DomainDN"
 
-    # Créer l'utilisateur
-    try {
 
-        New-ADUser `
-            -SamAccountName $samAccountName `
-            -UserPrincipalName "adrschultz@ekoloclast.local" `
-            -Name "$prenom $Nom" `
-            -GivenName $prenom `
-            -Surname $Nom `
-            -DisplayName "$prenom $Nom" `
-            -Department $Departement `
-            -Title $Service `
-            -Office $Site `
-            -Company $Societe `
-            -Path "OU=$Service,OU=$Departement,OU=LabUtilisateurs,$DomainDN" `
-            -AccountPassword (ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force) `
-            -Enabled $true `
-            -City $Site `
-            -Manager (Get-ADUser -Filter { GivenName -eq $Managerprenom -and Surname -eq $Managernom }).DistinguishedName `
-            -OfficePhone $Telephonefixe `
-            -MobilePhone $Telephoneportable `
-            -Description "non" `
-            -EmailAddress $mail
-        
-        Write-Output "Utilisateur $displayName créé avec succès."
-    }
-    catch {
-        Write-Error "Erreur lors de la création de l'utilisateur $displayName : $_"
-    }
+    # Vérifier si l'utilisateur existe déjà
+    if (-not (Get-ADUser -Filter { SamAccountName -eq $SamAccountName })) {
 
-    # Ajouter l'utilisateur aux groupes spécifiés
-    $groups = $user.FonctionModif -split "_"
-    foreach ($group in $groups) {
-        try {
-            Add-ADGroupMember -Identity $group -Members $samAccountName
-            Write-Output "Utilisateur $displayName ajouté au groupe $group."
+        # Créer l'utilisateur
+        if ($service.Length -ge 3) {
+            try {
+
+                New-ADUser `
+                    -SamAccountName $samAccountName `
+                    -UserPrincipalName $userPrincipalName `
+                    -Name "$prenom $Nom" `
+                    -GivenName $prenom `
+                    -Surname $Nom `
+                    -DisplayName "$prenom $Nom" `
+                    -Department $Departement `
+                    -Title $Service `
+                    -Office $Site `
+                    -Company $Societe `
+                    -Path "OU=$Service,OU=$Departement,OU=LabUtilisateurs,$DomainDN" `
+                    -AccountPassword (ConvertTo-SecureString "Azerty1*" -AsPlainText -Force) `
+                    -Enabled $true `
+                    -City $Site `
+                    -OfficePhone $Telephonefixe `
+                    -MobilePhone $Telephoneportable `
+                    -Description "non" `
+                    -EmailAddress $mail 
+                Write-host "Utilisateur $displayName cree avec succes." -ForegroundColor green
+            }
+            catch {
+                Write-host "Erreur lors de la creation de l'utilisateur $displayName : $_" -ForegroundColor red
+            }
         }
-        catch {
-            Write-Error "Erreur lors de l'ajout de l'utilisateur $displayName au groupe $group : $_"
+        else {
+            try {
+
+                New-ADUser `
+                    -SamAccountName $samAccountName `
+                    -UserPrincipalName $userPrincipalName `
+                    -Name "$prenom $Nom" `
+                    -GivenName $prenom `
+                    -Surname $Nom `
+                    -DisplayName "$prenom $Nom" `
+                    -Department $Departement `
+                    -Title $Service `
+                    -Office $Site `
+                    -Company $Societe `
+                    -Path "OU=$Departement,OU=LabUtilisateurs,$DomainDN" `
+                    -AccountPassword (ConvertTo-SecureString "Azerty1*" -AsPlainText -Force) `
+                    -Enabled $true `
+                    -City $Site `
+                    -OfficePhone $Telephonefixe `
+                    -MobilePhone $Telephoneportable `
+                    -Description "non" `
+                    -EmailAddress $mail 
+                Write-host "Utilisateur $displayName cree avec succes." -ForegroundColor green
+            }
+            catch {
+                Write-host "Erreur lors de la creation de l'utilisateur $displayName : $_" -ForegroundColor red
+            }
+        }
+    }
+
+    # Vérifier si l'utilisateur existe déjà
+    if (Get-ADUser -Filter { SamAccountName -eq $samAccountName }) {
+        # Ajouter l'utilisateur aux groupes spécifiés
+        $groups = $user.FonctionModif
+        foreach ($group in $groups) {
+            try {
+                Add-ADGroupMember -Identity $group -Members $samAccountName
+                Write-host "Utilisateur $displayName ajoute au groupe $group." -ForegroundColor Yellow
+            }
+            catch {
+                Write-host "Erreur lors de l'ajout de l'utilisateur $displayName au groupe $group : $_" -ForegroundColor red
+            }
         }
     }
 }
 
-Write-Output "Processus terminé."
+# Write-Output "Processus terminé."
