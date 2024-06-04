ecda8c48f81c3ad3ce437b3f533886695dab0b28
patrick baggiolini <136810328+Calllmeonichan@users.noreply.github.com>
2024-06-04 11:00:19 +0200

    Update Plagehoraire.ps1

--- S13/Plagehoraire.ps1
+++ S13/Plagehoraire.ps1
@@ -1,6 +1,6 @@
-$Ordinateurs = Import-Csv -Path "C:\Users\Administrator\Documents\Ordinateur.csv" | Select-Object -Skip 1
+$users = Import-Csv -Path "C:\Users\Administrator\Documents\Utilisateur.csv" | Select-Object -Skip 1
 
-foreach ($ordi in $Ordinateurs) {
+foreach ($user in $users) {
     $username = $user.Name  
     net user $username /times:M-F,7:30AM-8:00PM
 }
