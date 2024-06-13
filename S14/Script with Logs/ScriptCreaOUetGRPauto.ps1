ec44bd538a996a5983b9f84a47b9285a2ba89a79
Lucapouilly <160779668+Lucapouilly@users.noreply.github.com>
2024-06-13 11:36:05 +0200

    Update ScriptCreaOUetGRPauto.ps1

--- S14/Script with Logs/ScriptCreaOUetGRPauto.ps1
+++ S14/Script with Logs/ScriptCreaOUetGRPauto.ps1
@@ -4,6 +4,44 @@
 #                                            #
 ##############################################
 
+function Write-Log {
+    param (
+        [string]$scriptName,
+        [string]$message,
+        [string]$logDirectory = "C:\PowerShellLogs"
+    )
+
+    # Crée le répertoire de logs s'il n'existe pas
+    if (-not (Test-Path $logDirectory)) {
+        New-Item -ItemType Directory -Path $logDirectory
+    }
+
+    $logFile = Join-Path $logDirectory "$scriptName.log"
+
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $logMessage = "$timestamp - $message"
+    Add-Content -Path $logFile -Value $logMessage
+}
+
+function Log-Execution {
+    param (
+        [string]$scriptName,
+        [string]$modifications,
+        [string]$logDirectory = "C:\PowerShellLogs"
+    )
+
+    Write-Log -scriptName $scriptName -message "Script exécuté: $scriptName" -logDirectory $logDirectory
+    Write-Log -scriptName $scriptName -message "Modifications: $modifications" -logDirectory $logDirectory
+
+    # Enregistrement dans l'Observateur d'événements Windows
+    $eventSource = "PowerShellScript"
+    if (-not (Get-EventLog -LogName Application -Source $eventSource -ErrorAction SilentlyContinue)) {
+        New-EventLog -LogName Application -Source $eventSource
+    }
+
+    Write-EventLog -LogName Application -Source $eventSource -EventId 1 -EntryType Information -Message "Script exécuté: $scriptName - Modifications: $modifications"
+}
+
 
 # Chemin vers le fichier CSV
 #$FilePath = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
