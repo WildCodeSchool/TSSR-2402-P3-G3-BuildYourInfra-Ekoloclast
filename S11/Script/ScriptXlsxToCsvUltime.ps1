af78604a299c3cdf06617433e496a0ca85bad5a5
Nicolas Claverie <claverie.nicolas.lapo@gmail.com>
2024-06-13 15:26:37 +0200

    remodification de fichier xlsx to csv de la semaine 11

--- S11/Script/ScriptXlsxToCsvUltime.ps1
+++ S11/Script/ScriptXlsxToCsvUltime.ps1
@@ -7,8 +7,8 @@
 
 # Chemin d'accès du fichier XLSX d'entrée et du fichier CSV de sortie
 # A MODIFIER SI BESOIN 
-$fichierXLSX = "C:\Users\Nico\TSSR\Projet3\Script\s09_Ekoloclast.xlsx"
-$fichierCSV = "C:\Users\Nico\TSSR\Projet3\Script\s09_Ekoloclast_dossier.csv"
+$fichierXLSX = "C:\Users\Administrator\RessourcesScript\s09_Ekoloclast.xlsx"
+$fichierCSV = "C:\Users\Administrator\RessourcesScript\s09_Ekoloclast.csv"
 
 # Charger le contenu du fichier XLSX
 $data = Import-Excel -Path $fichierXLSX
@@ -26,6 +26,11 @@ function Remove-StringSpecialCharacters {
 
       $String = [Text.Encoding]::ASCII.GetString([Text.Encoding]::GetEncoding("Cyrillic").GetBytes($String))
 
+      $String = $String -replace '-', '' `
+         -replace ' ', '' `
+         -replace '/', '' `
+         -replace '\*', '' `
+         -replace "'", "" 
    }
 
    End {
